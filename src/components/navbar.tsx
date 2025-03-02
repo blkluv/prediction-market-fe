a0635a573ac416d9a732b3ea4bcd7b379380f684
Nestor Matias <nestormatias@Nestors-MBP.home>
2025-03-02 13:29:31 +0100

    fix: navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,40 +1,15 @@
-//import { client } from "@/app/client";
 "use client";
+
 import { client } from "@/app/client";
-import { createThirdwebClient } from "thirdweb";
 import { baseSepolia } from "thirdweb/chains";
 import { ConnectButton, lightTheme } from "thirdweb/react";
 
-import { inAppWallet, createWallet } from "thirdweb/wallets";
+import { inAppWallet } from "thirdweb/wallets";
 
-// const client = createThirdwebClient({
-//   clientId: "....",
-// });
-
-// const wallets = [
-//   inAppWallet({
-//     auth: {
-//       options: [
-//         "google",
-//         "discord",
-//         "telegram",
-//         "email",
-//         "x",
-//         "phone",
-//         "passkey",
-//       ],
-//     },
-//   }),
-//   createWallet("io.metamask"),
-//   createWallet("com.coinbase.wallet"),
-//   createWallet("me.rainbow"),
-//   createWallet("io.rabby"),
-//   createWallet("io.zerion.wallet"),
-// ];
 export function Navbar() {
   return (
     <div className="flex justify-between items-center mb-6">
-      <h1 className="text-2xl font-bold">Simple PRediction Market</h1>
+      <h1 className="text-2xl font-bold">Simple Prediction Market</h1>
       <div className="items-center flex gap-2">
         <ConnectButton
           client={client}
