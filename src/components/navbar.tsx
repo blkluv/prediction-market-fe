b156cc8b90e5578d721fe26665a56d97b3185d2a
Matt <solomatias321@gmail.com>
2025-03-02 14:05:02 +0100

    wip: fix connection using  pass keys

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,37 +1,13 @@
-//import { client } from "@/app/client";
 "use client";
+
 import { client } from "@/app/client";
-import { createThirdwebClient } from "thirdweb";
 import { baseSepolia } from "thirdweb/chains";
 import { ConnectButton, lightTheme } from "thirdweb/react";
 
-import { inAppWallet, createWallet } from "thirdweb/wallets";
+import { inAppWallet } from "thirdweb/wallets";
 
-// const client = createThirdwebClient({
-//   clientId: "....",
-// });
-
-// const wallets = [
-//   inAppWallet({
-//     auth: {
-//       options: [
-//         "google",
-//         "discord",
-//         "telegram",
-//         "email",
-//         "x",
-//         "phone",
-//         "passkey",
-//       ],
-//     },
-//   }),
-//   createWallet("io.metamask"),
-//   createWallet("com.coinbase.wallet"),
-//   createWallet("me.rainbow"),
-//   createWallet("io.rabby"),
-//   createWallet("io.zerion.wallet"),
-// ];
 export function Navbar() {
+  console.log("Navbar client", client);
   return (
     <div className="flex justify-between items-center mb-6">
       <h1 className="text-2xl font-bold">Simple PRediction Market</h1>
